1a33746f29e0c06489e3a8f38df3a082b25ee1ae
georgeblack <g30.l3g3nd@gmail.com>
2018-10-30 19:35:58 +0400

    new system for contact form (with ajax)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Portfolio Website
 
-Using the Freelancer Jekyll theme  [![Build Status](https://api.travis-ci.org/jeromelachaud/freelancer-theme.svg?branch=master)](https://travis-ci.org/jeromelachaud/freelancer-theme/) 
+Based on the Freelancer Jekyll theme  [![Build Status](https://api.travis-ci.org/jeromelachaud/freelancer-theme.svg?branch=master)](https://travis-ci.org/jeromelachaud/freelancer-theme/) 
 =========================
 
 Jekyll theme based on [Freelancer bootstrap theme ](http://startbootstrap.com/template-overviews/freelancer/)
--- js/contact_me_static.js
+++ js/contact_me_static.js
@@ -5,6 +5,43 @@ $(function() {
         submitError: function($form, event, errors) {
             // additional error messages or events
         },
+        submitSuccess: function($form, event) {
+            event.preventDefault();
+            
+            if($form.find('[name=honey]').val()) {
+                return;
+            }
+
+            var formData = new FormData();
+            formData.append('name',$form.find('[name=name]').val());
+            formData.append('email',$form.find('[name=email]').val());
+            formData.append('message',$form.find('[name=message]').val());
+            
+            $.ajax({
+                url: "https://script.google.com/macros/s/AKfycbyJYeDD-Lq57yfwpPCSz6QLI7gt34fQem5ctMK60Q/exec",
+                type: "POST",
+                processData: false,
+                contentType: false,
+                crossDomain: true,
+                data: formData,
+                
+                
+            }).done(function(resp) {
+                if(resp.result === 'success') {
+                    $form.find('#contact-error').hide();
+                    $form.find('[type=submit]').hide();
+                    $form.find('#contact-success').show();
+                }
+                else {
+                    $form.find('#contact-error').show();
+                }
+            }).error(function (xhr, status) {
+                $form.find('#contact-error').show();
+            });
+
+            
+            
+        },
 
         filter: function() {
             return $(this).is(":visible");
